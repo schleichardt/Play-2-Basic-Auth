0e24549bf0f7abb76a2be95cda375d5ec39bed03
Michael Schleichardt <michael@schleichardt.net>
2012-09-24 17:22:54 +0200

    setup publishing-configuration

--- Plugin/project/Build.scala
+++ Plugin/project/Build.scala
@@ -4,15 +4,19 @@ import PlayProject._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "Plugin"
-    val appVersion      = "1.0-SNAPSHOT"
+    val moduleOrganization = "info.schleichardt"
+    val appName = "Play-2-Basic-Auth"
+    val appVersion = "0.1-SNAPSHOT"
+    val publishingFolder = Path.userHome.absolutePath+"/Projekte/schleichardt.github.com/jvmrepo"
 
     val appDependencies = Seq(
       // Add your project dependencies here,
     )
 
     val main = PlayProject(appName, appVersion, appDependencies).settings(
-      // Add your own project settings here      
+      organization := moduleOrganization,
+      publishTo := Some(Resolver.file("file",  new File(publishingFolder))(Resolver.mavenStylePatterns)), 
+      publishMavenStyle := true
     )
 
 }
