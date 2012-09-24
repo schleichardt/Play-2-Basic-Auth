2fd4b28af8fddbd4b972b53c9ba5bbe03aa929d0
Michael Schleichardt <michael@schleichardt.net>
2012-09-24 21:31:19 +0200

    configured test app for module

--- TestApp/app/Global.scala
+++ TestApp/app/Global.scala
@@ -1,3 +1,12 @@
-object Global {
+import info.schleichardt.play2.basicauth.CredentialsFromConfCheck
+import play.api.GlobalSettings
+import play.api.mvc.{Handler, Action, Results, RequestHeader}
+import play.api.mvc.Results.Unauthorized
+import info.schleichardt.play2.basicauth.BasicAuth._
 
+object Global extends GlobalSettings {
+  override def onRouteRequest(request: RequestHeader) =
+    requireBasicAuthentication(request, new CredentialsFromConfCheck) {
+      super.onRouteRequest(request)
+    }
 }
--- TestApp/project/Build.scala
+++ TestApp/project/Build.scala
@@ -4,15 +4,15 @@ import PlayProject._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "TestApp"
-    val appVersion      = "1.0-SNAPSHOT"
+  val appName = "TestApp"
+  val appVersion = "1.0-SNAPSHOT"
 
-    val appDependencies = Seq(
-      // Add your project dependencies here,
-    )
+  val appDependencies = Seq(
+    "info.schleichardt" %% "play-2-basic-auth" % "0.1-SNAPSHOT"
+  )
 
-    val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-      // Add your own project settings here      
-    )
+  val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
+    // Add your own project settings here
+  )
 
 }
