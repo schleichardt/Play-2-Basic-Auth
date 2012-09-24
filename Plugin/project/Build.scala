6010b7944bb6206b45c5b10f44e01089000ae532
Michael Schleichardt <michael@schleichardt.net>
2012-09-24 21:01:00 +0200

    reformat

--- Plugin/project/Build.scala
+++ Plugin/project/Build.scala
@@ -4,19 +4,18 @@ import PlayProject._
 
 object ApplicationBuild extends Build {
 
-    val moduleOrganization = "info.schleichardt"
-    val appName = "play-2-basic-auth"
-    val appVersion = "0.1-SNAPSHOT"
-    val publishingFolder = Path.userHome.absolutePath+"/Projekte/schleichardt.github.com/jvmrepo"
+  val moduleOrganization = "info.schleichardt"
+  val appName = "play-2-basic-auth"
+  val appVersion = "0.1-SNAPSHOT"
+  val publishingFolder = Path.userHome.absolutePath + "/Projekte/schleichardt.github.com/jvmrepo"
 
-    val appDependencies = Seq(
-      // Add your project dependencies here,
-    )
+  val appDependencies = Seq(
+  )
 
-    val main = PlayProject(appName, appVersion, appDependencies).settings(
-      organization := moduleOrganization,
-      publishTo := Some(Resolver.file("file",  new File(publishingFolder))(Resolver.mavenStylePatterns)), 
-      publishMavenStyle := true
-    )
+  val main = PlayProject(appName, appVersion, appDependencies).settings(
+    organization := moduleOrganization,
+    publishTo := Some(Resolver.file("file", new File(publishingFolder))(Resolver.mavenStylePatterns)),
+    publishMavenStyle := true
+  )
 
 }
