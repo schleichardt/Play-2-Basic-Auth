07fd6dd948e3514f553a1bbb447ab2f188ac3d14
Michael Schleichardt <michael@schleichardt.net>
2012-12-29 23:04:01 +0100

    update API usage ExampleApp

--- TestApp/app/Global.scala
+++ TestApp/app/Global.scala
@@ -1,8 +1,11 @@
 import play.api.GlobalSettings
+import play.api.mvc.RequestHeader
+import info.schleichardt.play2.api.basicauth.CredentialsFromConfChecker
+import info.schleichardt.play2.api.basicauth.Authenticator
 
 object Global extends GlobalSettings {
 
   val requireBasicAuthentication = Authenticator(new CredentialsFromConfChecker)
 
-  override def onRouteRequest(request: RequestHeader) = requireBasicAuthentication(request, super.onRouteRequest(request))
+  override def onRouteRequest(request: RequestHeader) = requireBasicAuthentication(request, () => super.onRouteRequest(request))
 }
